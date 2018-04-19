164efacc875ae4d3896a7e880ac834b55a5ac9f0
taele <tlee95@illinois.edu>
2018-04-19 18:53:03 -0500

    initial

---
+++ app/src/main/java/com/example/taele/finalproject/DisplayRecipes.java
@@ -0,0 +1,13 @@
+package com.example.taele.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_recipes);
+    }
+}
