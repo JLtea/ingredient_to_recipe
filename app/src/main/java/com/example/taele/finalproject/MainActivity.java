923b9aff2138dca41a63b5d1a1d75b0d8bc7fbd0
taele <tlee95@illinois.edu>
2018-04-24 14:33:47 -0500

    initial

--- app/src/main/java/com/example/taele/finalproject/MainActivity.java
+++ app/src/main/java/com/example/taele/finalproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button apple = findViewById(R.id.button2);
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecipe("Apple");
+            }
+        });
+        final Button potato = findViewById(R.id.button3);
+        potato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecipe("Potato");
+            }
+        });
+        final Button spinach = findViewById(R.id.button4);
+        spinach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecipe("Spinach");
+            }
+        });
     }
-    public void ButtonTap(View view) {
+    public void startRecipe(String key) {
         Intent intent = new Intent(this, DisplayRecipes.class);
+        intent.putExtra("key", key);
         startActivity(intent);
     }
 }
