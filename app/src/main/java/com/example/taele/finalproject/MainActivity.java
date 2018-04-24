9d24ff343b798d82d8eb97c160bff80e91658697
zoufaith <yuchenz4@illinois.edu>
2018-04-24 17:20:03 -0500

    explore button

--- app/src/main/java/com/example/taele/finalproject/MainActivity.java
+++ app/src/main/java/com/example/taele/finalproject/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.taele.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LeftoverRecipesApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+                Log.i(TAG,"ExploringRecipes!!");
+                Toast.makeText(getApplicationContext(),"Got any leftover?",Toast.LENGTH_SHORT )
+                        .show();
+            }
+        });
+    } public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
